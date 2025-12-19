86a1307471313a39b98d23453d59245d624cca17
gros0591 <gros0591@d.umn.edu>
2025-12-19 14:33:49 -0600

    carousel modifications

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -5,7 +5,7 @@ import "./Carousel.css";
 
 const Carousel = () => {
   return (
-    <div id="mainCarousel" className="carousel slide carousel-fade">
+    <div id="mainCarousel" className="carousel slide carousel-fade" data-bs-ride="carousel">
       <div className="carousel-inner">
         <div className="carousel-item active c-item">
           <img src={Cleaner} className="d-block w-100 c-img"></img>
@@ -35,24 +35,6 @@ const Carousel = () => {
           </div>
         </div>
       </div>
-      <button
-        className="carousel-control-prev"
-        type="button"
-        data-bs-target="#mainCarousel"
-        data-bs-slide="prev"
-      >
-        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span className="visually-hidden">Previous</span>
-      </button>
-      <button
-        className="carousel-control-next"
-        type="button"
-        data-bs-target="#mainCarousel"
-        data-bs-slide="next"
-      >
-        <span className="carousel-control-next-icon" aria-hidden="true"></span>
-        <span className="visually-hidden">Next</span>
-      </button>
     </div>
   );
 };
