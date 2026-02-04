0a888b95fb96fe52ee3906fa9ff7bc14ba5fe980
gros0591 <gros0591@d.umn.edu>
2026-02-04 17:02:32 -0600

    Added Grid for industries served information

--- src/Home.tsx
+++ src/Home.tsx
@@ -24,13 +24,51 @@ const Home = () => {
       <div>
         <div className="container-fluid text-center t-size">
           <div className="row r-color">
-            <div className="pad">
+            <div style={{ marginTop: "25px", marginBottom: "25px" }}>
               <text>
                 Here at Multipro, we do it all! Everything from interim staffing
                 to unit turnovers. <br></br>
                 <strong>We are on your side!</strong>
               </text>
             </div>
+          </div>
+        </div>
+        <div
+          className="container-fluid text-center"
+          style={{ marginTop: "30px", marginBottom: "30px" }}
+        >
+          <strong
+            style={{
+              fontSize: "27pt",
+              letterSpacing: "1.25pt",
+              wordSpacing: "1.75pt",
+            }}
+          >
+            Industries We Serve
+          </strong>
+        </div>
+      </div>
+      <div className="container px-4 text-center">
+        <div className="row row-cols-3 gx-5 gy-5">
+          <div className="col">
+            <div className="p-3 grid">Multi-Family & Apartment Communities</div>
+          </div>
+          <div className="col">
+            <div className="p-3 grid">Commercial & Retail Properties</div>
+          </div>
+          <div className="col">
+            <div className="p-3 grid">HOA & Condo Associations</div>
+          </div>
+          <div className="col">
+            <div className="p-3 grid">Property Management Companies</div>
+          </div>
+          <div className="col">
+            <div className="p-3 grid">
+              Senior Living & Assisted Living Communities
+            </div>
+          </div>
+          <div className="col">
+            <div className="p-3 grid">Industrial & Warehouse Facilities</div>
           </div>
         </div>
       </div>
