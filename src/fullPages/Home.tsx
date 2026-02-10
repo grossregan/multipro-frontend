a2a11ec2bc6a7d0b4798aaea98183152fcdb788d
gros0591 <gros0591@d.umn.edu>
2026-02-10 15:33:19 -0600

    refactor gridCard intoits own file: GridCard

--- src/fullPages/Home.tsx
+++ src/fullPages/Home.tsx
@@ -6,23 +6,9 @@ import SeniorLiving from "../assets/assistedLiving.webp";
 import Warehouse from "../assets/warehouse.webp";
 
 import Carousel from "../components/Carousel";
+import GridCard from "../components/GridCard";
 import "./Home.css";
 
-interface CardDetails {
-  industry: string;
-  img: string;
-}
-
-function gridCard(details: CardDetails) {
-  return (
-    <div className="card">
-      <img src={details.img} className="card-img-top" alt="" />
-      <div className="card-body p-3 grid">
-        <h5 className="card-title">{details.industry}</h5>
-      </div>
-    </div>
-  );
-}
 
 const Home = () => {
   return (
@@ -62,34 +48,34 @@ const Home = () => {
       <div className="container-fluid container-grid text-center">
         <div className="row row-cols-3 gx-4 gy-5">
           <div className="col">
-            {gridCard({
+            {GridCard({
               industry: "Multi-Family & Apartment Communities",
               img: Apartment,
             })}
           </div>
           <div className="col">
-            {gridCard({
+            {GridCard({
               industry: "Commercial & Retail Properties",
               img: Retail,
             })}
           </div>
           <div className="col">
-            {gridCard({ industry: "HOA & Condo Associations", img: Hoa })}
+            {GridCard({ industry: "HOA & Condo Associations", img: Hoa })}
           </div>
           <div className="col">
-            {gridCard({
+            {GridCard({
               industry: "Property Management Companies",
               img: PropertyManager,
             })}
           </div>
           <div className="col">
-            {gridCard({
+            {GridCard({
               industry: "Senior Living & Assisted Living Communities",
               img: SeniorLiving,
             })}
           </div>
           <div className="col">
-            {gridCard({
+            {GridCard({
               industry: "Industrial & Warehouse Facilities",
               img: Warehouse,
             })}
