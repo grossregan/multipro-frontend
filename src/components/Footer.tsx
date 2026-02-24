01053ee4d9825ea7b21820845760872cd2a223d5
gros0591 <gros0591@d.umn.edu>
2026-02-24 11:28:39 -0600

    rough form visuals

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,16 +1,59 @@
+import './Footer.css';
+
 const Footer = () => {
   return (
     <>
       <div
         className="container-fluid text-center d-flex align-items-center flex-column"
         style={{
-          height: "375px",
+          height: "auto",
           backgroundColor: "#a6a6a6",
           color: "black",
         }}
       >
-        <div className="m-auto">
-          <h1 className="fw-bolder fs-1">Footer</h1>
+        <h1 style={{ paddingTop: "10px" }}>Contact Us!</h1>
+        <div className="my-auto">
+          <div className="row">
+            <div className="col">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="First name"
+                aria-label="First name"
+              />
+            </div>
+            <div className="col">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Last name"
+                aria-label="Last name"
+              />
+            </div>
+          </div>
+          <div className="row my-3">
+            <div className="col">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Email Address"
+                aria-label="Email Address"
+              />
+            </div>
+          </div>
+          <div className="row my-3">
+            <div className="col">
+              <textarea
+                className="form-control"
+                placeholder="Type your message here"
+                id="Message entry"
+                rows={6}
+              />
+            </div>
+          </div>
+          <div className="row my-3">
+            <button type="button" className="btn btn-outline-primary">Submit</button>
+          </div>
         </div>
       </div>
     </>
