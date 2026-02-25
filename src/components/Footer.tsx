bb2ea62f0d4a4cd4018cca61d154fd426b195ce0
gros0591 <gros0591@d.umn.edu>
2026-02-25 09:43:36 -0600

    contact format

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -11,51 +11,53 @@ const Footer = () => {
           color: "black",
         }}
       >
-        <h1 style={{ paddingTop: "10px" }}>Contact Us!</h1>
-        <div className="my-auto">
-          <div className="row">
-            <div className="col">
-              <input
-                type="text"
-                className="form-control"
-                placeholder="First name"
-                aria-label="First name"
-              />
+        <div className='col offset-md-6'>
+          <h1 style={{ paddingTop: "10px" }}>Contact Us!</h1>
+          <div className="my-auto">
+            <div className="row">
+              <div className="col">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="First name"
+                  aria-label="First name"
+                />
+              </div>
+              <div className="col">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Last name"
+                  aria-label="Last name"
+                />
+              </div>
             </div>
-            <div className="col">
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Last name"
-                aria-label="Last name"
-              />
+            <div className="row my-3">
+              <div className="col">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Email Address"
+                  aria-label="Email Address"
+                />
+              </div>
+            </div>
+            <div className="row my-3">
+              <div className="col">
+                <textarea
+                  className="form-control"
+                  placeholder="Type your message here"
+                  id="Message entry"
+                  rows={6}
+                />
+              </div>
+            </div>
+            <div className="row my-3">
+              <button type="button" className="btn btn-outline-primary">Submit</button>
             </div>
           </div>
-          <div className="row my-3">
-            <div className="col">
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Email Address"
-                aria-label="Email Address"
-              />
-            </div>
-          </div>
-          <div className="row my-3">
-            <div className="col">
-              <textarea
-                className="form-control"
-                placeholder="Type your message here"
-                id="Message entry"
-                rows={6}
-              />
-            </div>
-          </div>
-          <div className="row my-3">
-            <button type="button" className="btn btn-outline-primary">Submit</button>
-          </div>
+                </div>
         </div>
-      </div>
     </>
   );
 };
