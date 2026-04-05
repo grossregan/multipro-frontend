bf7acb8511518152a2e94dbd328c1ef8d298be1a
gros0591 <gros0591@d.umn.edu>
2026-04-05 13:45:48 -0500

    add site map column to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,13 +5,19 @@ const Footer = () => {
   return (
     <>
       <div
-        className="container-fluid text-center d-flex align-items-center flex-column"
+        className="container-fluid text-center d-flex align-items-center"
         style={{
           height: "auto",
           backgroundColor: "#a6a6a6",
           color: "black",
         }}
       >
+        <div className="col">
+          <div className="row">
+            <h6>Site Map</h6>
+          </div>
+          <div className="col" />
+        </div>
         <div className="col">
           <ContactForm />
         </div>
