9aa4647bfadf578f02a7f69e4b56ed8667444eb7
gros0591 <gros0591@d.umn.edu>
2026-04-09 08:37:30 -0500

    minimal site map

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,13 @@
 import ContactForm from "./ContactForm";
+import { Link } from "react-router-dom";
+
 import "./Footer.css";
 
 const Footer = () => {
   return (
     <>
       <div
-        className="container-fluid text-center d-flex align-items-center"
+        className="container-fluid d-flex align-items-center"
         style={{
           height: "auto",
           backgroundColor: "#a6a6a6",
@@ -13,12 +15,19 @@ const Footer = () => {
         }}
       >
         <div className="col">
-          <div className="row">
-            <h6>Site Map</h6>
+          <div className="container-fluid">
+            <h4 className="text-center">Site Map</h4>
+            <ul>
+              <Link to="/"><li>Home</li></Link>
+              <Link to="/services"><li>Services</li></Link>
+              <Link to="/industries"><li>Industries</li></Link>
+              <Link to="/about"><li>About</li></Link>
+              <Link to="/faq"><li>FAQ</li></Link>
+            </ul>
           </div>
-          <div className="col" />
         </div>
-        <div className="col">
+        <div className="col" />
+        <div className="col text-center">
           <ContactForm />
         </div>
       </div>
