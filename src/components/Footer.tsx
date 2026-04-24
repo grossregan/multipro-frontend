8ca377aa0c91f7dd299233ba54fc3ac218634350
gros0591 <gros0591@d.umn.edu>
2026-04-24 10:42:09 -0500

    add RENET logo

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,7 @@
 import ContactForm from "./ContactForm";
 import { Link } from "react-router-dom";
 import Multipro from "../assets/MMS_Logo.png";
+import Renet from "../assets/RENET_MemberLogo FullColor.png";
 
 import "./Footer.css";
 
@@ -17,12 +18,15 @@ const Footer = () => {
       >
         <div className="row pt-1 p-4 mx-auto">
           <div className="col my-auto p-4">
-            <div className="vstack text-center gap-3" style={{fontSize:"16pt", fontFamily:"north-rock"}}>
+            <div
+              className="vstack text-center gap-3"
+              style={{ fontSize: "16pt", fontFamily: "north-rock" }}
+            >
               <img
                 src={Multipro}
                 alt="multipro"
-                height={288.5}
-                width={488.5}
+                height={238.5}
+                width={408.5}
                 className="mx-auto"
               />
               Solutions For All Your Property Needs
@@ -33,28 +37,28 @@ const Footer = () => {
           </div>
           <div className="col text-center my-auto mx-auto p-4">
             <h4>Site Map</h4>
-            <Link to="/">
-              Home
-            </Link><br />
-            <Link to="/services">
-              Services
-            </Link><br />
-            <Link to="/industries">
-              Industries
-            </Link><br />
-            <Link to="/about">
-              About
-            </Link><br />
-            <Link to="/faq">
-              FAQ
-            </Link><br />
+            <Link to="/">Home</Link>
+            <br />
+            <Link to="/services">Services</Link>
+            <br />
+            <Link to="/industries">Industries</Link>
+            <br />
+            <Link to="/about">About</Link>
+            <br />
+            <Link to="/faq">FAQ</Link>
+            <br />
           </div>
           <div className="w-100" />
-          <div className="col-12 text-center pb-2 footer-center">
+          <div className="col">
+            <img alt="RENET Logo" src={Renet} style={{ width: "50%", alignSelf:"center"}} />
+          </div>
+          <div className="col pb-2 text-center footer-center">
             DAN@MULTIPROMN.COM
-            <br />Direct- 612-405-8050
+            <br />
+            Direct- 612-405-8050
             <br />© 2025 by Multipro Maintenance Solutions LLC.
           </div>
+          <div className="col w-100" />
         </div>
       </div>
     </>
