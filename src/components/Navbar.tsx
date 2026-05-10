1013e62ecdf1ca4ef68d4adab6f82499fd485de8
gros0591 <gros0591@d.umn.edu>
2026-05-10 15:22:34 -0500

    mobile navbar button fix

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,32 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Navbar as BootstrapNavbar, Nav } from "react-bootstrap";
+import { Navbar as BootstrapNavbar, Container, Nav } from "react-bootstrap";
 import "./Navbar.css";
 import Brand from "../assets/MMS_Horizontal.png"
 
 
 const Navbar: React.FC = () => {
   return (
-    <div className="container nav-container">
-      <BootstrapNavbar id="main-navbar" className="navbar fixed-top navbar-expand-lg mt-2">
+    <Container>
+      <BootstrapNavbar expand="lg" id="main-navbar" className="navbar fixed-top navbar-expand-lg mt-2">
         <div className="container-fluid">
           <BootstrapNavbar.Brand as={Link} className="navbar-brand nav-item" to="/">
             <img src={Brand} alt="Multipro logo" height={48}/>
           </BootstrapNavbar.Brand>
 
-          <button
-            className="navbar-toggler toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
+          <BootstrapNavbar.Toggle />
 
-          <BootstrapNavbar.Collapse className="collapse navbar-collapse" id="navbarNav">
+          <BootstrapNavbar.Collapse className="rounded" id="navbarNav">
             <Nav className="navbar-nav">
               <Nav.Item className="nav-item me-3">
                 <Link className="nav-link" to="/services">
@@ -53,7 +43,7 @@ const Navbar: React.FC = () => {
           </BootstrapNavbar.Collapse>
         </div>
       </BootstrapNavbar>
-    </div>
+    </Container>
   );
 };
 
